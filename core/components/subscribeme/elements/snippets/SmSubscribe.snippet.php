e836e7c831019884456f5e29e1d996369fb7030f
bobray <bobray@softville.com>
2012-03-09 01:27:53 -0500

    removed login-logout link completely

--- core/components/subscribeme/elements/snippets/SmSubscribe.snippet.php
+++ core/components/subscribeme/elements/snippets/SmSubscribe.snippet.php
@@ -1,4 +1,3 @@
-<?php
 /**
      * SubscribeMe
      * Copyright 2012 Bob Ray <http://bobsguides/com>
@@ -39,11 +38,24 @@
   $noShows = array($sp['loginPageId'],$sp['registerPageId']);
   $noShows = array_merge($noShows,explode(',', $sp['noShow']));
 
-if ($modx->user->hasSessionContext($modx->context->get('key')) || in_array($modx->resource->get('id'),$noShows) ) {
-   return '<a href="[[!~' . $scriptProperties['loginPageId'] . ']]? &service=logout" style="padding-left:30px;color:lavender">Log out</a>';
+
+/* don't show subscribe request is user is logged in or
+ * current page is in noShow list
+*/
+
+$docId = $modx->resource->get('id');
+
+/* Just show login_logout button (if requested)
+ if we're not showing the subscribe request */
+$loggedIn = $modx->user->hasSessionContext($modx->context->get('key'));
+if ($loggedIn || in_array($docId,$noShows) ) {
+       /* <br /> maintains page layout. Change to '' if necessary for your layout */
+       return '<br />';
+
 } else {
-   $modx->regClientCSS(MODX_ASSETS_URL . 'components/subscribeme/css/subscribeme.css');
-   $modx->regClientCSS('http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.1/themes/base/jquery-ui.css');
+   /* Load CSS and JS, and show the subscribe request */
+    $modx->regClientCSS(MODX_ASSETS_URL . 'components/subscribeme/css/subscribeme.css');
+    $modx->regClientCSS('http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.1/themes/base/jquery-ui.css');
     $modx->regClientStartupScript('http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js');
     $modx->regClientStartupScript('http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js');
     
@@ -53,13 +65,13 @@ if ($modx->user->hasSessionContext($modx->context->get('key')) || in_array($modx
 
     $fields['whyDialogHeight'] = $modx->getOption('whyDialogHeight',$sp, 475);
     $fields['whyDialogWidth'] = $modx->getOption('whyDialogWidth',$sp, 300);
-    $fields['whyDialogTop'] = $modx->getOption('whyDialogTop',$sp, 55);
-    $fields['whyDialogLeft'] = $modx->getOption('whyDialogLeft',$sp, 212);
+    $fields['whyDialogTop'] = $modx->getOption('whyDialogTop',$sp, 40);
+    $fields['whyDialogLeft'] = $modx->getOption('whyDialogLeft',$sp, 202);
 
     $fields['privacyDialogHeight'] = $modx->getOption('privacyDialogHeight',$sp, 500);
     $fields['privacyDialogWidth'] = $modx->getOption('privacyDialogWidth',$sp, 300);
-    $fields['privacyDialogTop'] = $modx->getOption('privacyDialogTop',$sp, 55);
-    $fields['privacyDialogLeft'] = $modx->getOption('privacyDialogLeft',$sp, 347);
+    $fields['privacyDialogTop'] = $modx->getOption('privacyDialogTop',$sp, 40);
+    $fields['privacyDialogLeft'] = $modx->getOption('privacyDialogLeft',$sp, 340);
 
 
     return $modx->getChunk('SmSubscribe', $fields) ;
