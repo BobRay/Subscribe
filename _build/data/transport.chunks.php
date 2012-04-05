52da22b6c33e5c1e71194fe1c8c30471f901fa80
bobray <bobray@softville.com>
2012-04-05 14:08:01 -0400

    put Tpl postfix on all chunks

--- _build/data/transport.chunks.php
+++ _build/data/transport.chunks.php
@@ -13,73 +13,73 @@ $chunks[1] ->fromArray(array(
 $chunks[2] = $modx->newObject('modChunk');
 $chunks[2] ->fromArray(array(
     'id' => 2,
-    'name' => 'SmRegisterForm',
+    'name' => 'SmRegisterFormTpl',
     'description' => 'Chunk containing Subscribe register form',
-    'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/smregisterform.chunk.html'),
+    'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/smregisterformtpl.chunk.html'),
     'properties' => '',
 ),'',true,true);
 $chunks[3] = $modx->newObject('modChunk');
 $chunks[3] ->fromArray(array(
     'id' => 3,
-    'name' => 'SmSubscribe',
+    'name' => 'SmSubscribeTpl',
     'description' => 'Subscribe extra chunk containing request to subscribe',
-    'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/smsubscribe.chunk.html'),
+    'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/smsubscribetpl.chunk.html'),
     'properties' => '',
 ),'',true,true);
 $chunks[4] = $modx->newObject('modChunk');
 $chunks[4] ->fromArray(array(
     'id' => 4,
-    'name' => 'SmRegisterJS',
+    'name' => 'SmRegisterJsTpl',
     'description' => 'Subscribe extra chunk containing JS code to validate the Register form',
-    'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/smregisterjs.chunk.html'),
+    'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/smregisterjstpl.chunk.html'),
     'properties' => '',
 ),'',true,true);
 $chunks[5] = $modx->newObject('modChunk');
 $chunks[5] ->fromArray(array(
     'id' => 5,
-    'name' => 'SmLogoutLink',
+    'name' => 'SmLogoutLinkTpl',
     'description' => 'Subscribe extra logout link',
-    'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/smlogoutlink.chunk.html'),
+    'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/smlogoutlinktpl.chunk.html'),
     'properties' => '',
 ),'',true,true);
 $chunks[6] = $modx->newObject('modChunk');
 $chunks[6] ->fromArray(array(
     'id' => 6,
-    'name' => 'SmManagePrefsLink',
+    'name' => 'SmManagePrefsLinkTpl',
     'description' => 'Chunk containing request to subscribe',
-    'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/smmanageprefslink.chunk.html'),
+    'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/smmanageprefslinktpl.chunk.html'),
     'properties' => '',
 ),'',true,true);
 $chunks[7] = $modx->newObject('modChunk');
 $chunks[7] ->fromArray(array(
     'id' => 7,
-    'name' => 'SmManagePrefsForm',
+    'name' => 'SmManagePrefsFormTpl',
     'description' => 'Subscribe extra Form for users to manage preferences',
-    'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/smmanageprefsform.chunk.html'),
+    'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/smmanageprefsformtpl.chunk.html'),
     'properties' => '',
 ),'',true,true);
 $chunks[8] = $modx->newObject('modChunk');
 $chunks[8] ->fromArray(array(
     'id' => 8,
-    'name' => 'SmUnsubscribeMessage',
+    'name' => 'SmUnsubscribeMessageTpl',
     'description' => 'Subscribe extra chunk containing unsubscribe success message',
-    'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/smunsubscribemessage.chunk.html'),
+    'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/smunsubscribemessagetpl.chunk.html'),
     'properties' => '',
 ),'',true,true);
 $chunks[9] = $modx->newObject('modChunk');
 $chunks[9] ->fromArray(array(
     'id' => 9,
-    'name' => 'SmChangePrefsSuccessMessage',
+    'name' => 'SmChangePrefsSuccessMessageTpl',
     'description' => 'Success message for Subscribe change preferences form',
-    'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/smchangeprefssuccessmessage.chunk.html'),
+    'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/smchangeprefssuccessmessagetpl.chunk.html'),
     'properties' => '',
 ),'',true,true);
 $chunks[10] = $modx->newObject('modChunk');
 $chunks[10] ->fromArray(array(
     'id' => 10,
-    'name' => 'SmInterestList',
+    'name' => 'SmInterestListTpl',
     'description' => 'Interest list checkboxes for Subscribe Register and Manage Preferences forms.',
-    'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/sminterestlist.chunk.html'),
+    'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/sminterestlisttpl.chunk.html'),
     'properties' => '',
 ),'',true,true);
 return $chunks;
