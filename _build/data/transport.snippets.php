68fefe16dbe5f399cb77b3866c1eb36176703dd4
bobray <bobray@softville.com>
2012-04-05 15:04:38 -0400

    fixed bug with failure to remove php tag in transport.snippets

--- _build/data/transport.snippets.php
+++ _build/data/transport.snippets.php
@@ -1,5 +1,11 @@
 <?php
-
+function getSnippetContent($filename) {
+    $o = file_get_contents($filename);
+    $o = str_replace('<?php','',$o);
+    $o = str_replace('?>','',$o);
+    $o = trim($o);
+    return $o;
+}
 $snippets = array();
 
 $snippets[1] = $modx->newObject('modSnippet');
@@ -7,7 +13,7 @@ $snippets[1] ->fromArray(array(
     'id' => 1,
     'name' => 'SbsGetInterests',
     'description' => "Subscribe extra - Gets user interests from Register form or Manage Preferences form and saves them in the 'comment' field of the User Profile.",
-    'snippet' => file_get_contents($sources['source_core'].'/elements/snippets/sbsgetinterests.snippet.php'),
+    'snippet' => getSnippetContent($sources['source_core'].'/elements/snippets/sbsgetinterests.snippet.php'),
     'properties' => '',
 ),'',true,true);
 $snippets[2] = $modx->newObject('modSnippet');
@@ -15,7 +21,7 @@ $snippets[2] ->fromArray(array(
     'id' => 2,
     'name' => 'SbsSubscribe',
     'description' => "Subscribe - Displays request to subscribe if user is not logged in or Logout  and Manage Preferences links",
-    'snippet' => file_get_contents($sources['source_core'].'/elements/snippets/sbssubscribe.snippet.php'),
+    'snippet' => getSnippetContent($sources['source_core'].'/elements/snippets/sbssubscribe.snippet.php'),
     'properties' => '',
 ),'',true,true);
 $snippets[3] = $modx->newObject('modSnippet');
@@ -23,7 +29,7 @@ $snippets[3] ->fromArray(array(
     'id' => 3,
     'name' => 'SbsLoadRegisterJS',
     'description' => "Subscribe - Loads JS to validate registration form",
-    'snippet' => file_get_contents($sources['source_core'].'/elements/snippets/sbsloadregisterjs.snippet.php'),
+    'snippet' => getSnippetContent($sources['source_core'].'/elements/snippets/sbsloadregisterjs.snippet.php'),
     'properties' => '',
 ),'',true,true);
 $snippets[4] = $modx->newObject('modSnippet');
@@ -31,7 +37,7 @@ $snippets[4] ->fromArray(array(
     'id' => 4,
     'name' => 'SbsGetCurrentPrefs',
     'description' => "Subscribe extra snippet to populate user's current preferences",
-    'snippet' => file_get_contents($sources['source_core'].'/elements/snippets/sbsgetcurrentprefs.snippet.php'),
+    'snippet' => getSnippetContent($sources['source_core'].'/elements/snippets/sbsgetcurrentprefs.snippet.php'),
     'properties' => '',
 ),'',true,true);
 return $snippets;
