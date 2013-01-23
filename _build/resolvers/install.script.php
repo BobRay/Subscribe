f1ece7945da0e8857881d44abf796f82156c69b4
bobray <bobray@softville.com>
2013-01-22 23:54:26 -0500

    tweaks to resolver

--- _build/resolvers/install.script.php
+++ _build/resolvers/install.script.php
@@ -52,110 +52,115 @@ switch($options[xPDOTransport::PACKAGE_ACTION]) {
     case xPDOTransport::ACTION_INSTALL:
     case xPDOTransport::ACTION_UPGRADE:
 
-    /* @var $resource modResource */
-    /* @var $setting modSystemSetting */
+        /* @var $resource modResource */
+        /* @var $setting modSystemSetting */
 
-    $parent = null;
-    $defaultTemplate = $modx->getOption('default_template',null);
+        $parent = null;
+        $defaultTemplate = $modx->getOption('default_template',null);
 
-    /* get subscribe folder */
-    /* @var $folder modResource */
-    $folder = $modx->getObject('modResource', array ('alias' => 'subscribe-folder'));
+        /* get subscribe folder */
+        /* @var $folder modResource */
+        $folder = $modx->getObject('modResource', array ('alias' => 'subscribe-folder'));
 
-    if (!$folder) {
-        $modx->log(xPDO::LOG_LEVEL_ERROR,'Could not find subscribe-folder resource');
-        $folderId = null;
-    } else {
-        $folderId = $folder->get('id');
-        $folder->set('template', $defaultTemplate);
-        $folder->save();
-    }
-
-    /* look for a login page and set the System Setting for it. */
-    /* @var $loginPage modResource */
-    $loginPage = $modx->getObject('modResource',array('alias' => 'login'));
-    if (! $loginPage) {
-        $loginPage = $modx->getObject('modResource',array('alias' => 'Login'));
-    }
-    if (! $loginPage) {
-        $modx->log(xPDO::LOG_LEVEL_ERROR, 'Login page not found - sbs_login_page_id System Settings must be set manually');
-    } else {
-        /* @var $ss modSystemSetting */
-        $ss = $modx->getObject('modSystemSetting', array('key' => 'sbs_login_page_id'));
-        if (!$ss) {
-            $modx->log(xPDO::LOG_LEVEL_ERROR, 'sbs_login_page_id System Setting not found');
+        if (!$folder) {
+            $modx->log(xPDO::LOG_LEVEL_ERROR,'Could not find subscribe-folder resource');
+            $folderId = null;
         } else {
-            $ss->set('value', $loginPage->get('id'));
-            if ($ss->save()) {
-                $modx->log(xPDO::LOG_LEVEL_INFO, 'set sbs_login_page_id System Setting to ID of Login page');
-            }
+            $folderId = $folder->get('id');
+            $folder->set('template', $defaultTemplate);
+            $folder->save();
         }
-    }
-   /* set other System Setting values to resource IDs of the subscribe resources
-    * this array is in the form 'alias' =>
 
-   */
-    $settingNames = array(
-         'confirm-register' => 'sbs_confirm_register_page_id',
-         'manage-preferences' => 'sbs_manage_prefs_page_id',
-         'subscribe' => 'sbs_register_page_id',
-         'registration-confirmed' => 'sbs_registration_confirmed_page_id',
-         'thanks-for-registering' => 'sbs_thank_you_page_id',
-         'unsubscribe' => 'sbs_unsubscribe_page_id',
-    );
-
-    foreach($settingNames as $key => $value) {
-        /* @var $systemSetting modSystemSetting */
-        $resource = $modx->getObject('modResource', array ('alias' => $key));
-        $systemSetting = $modx->getObject('modSystemSetting', array ('key' => $value));
-
-        if ($resource && $systemSetting) {
-            $systemSetting->set('value', $resource->get('id'));
-            if ($systemSetting->save()) {
-                $modx->log(xPDO::LOG_LEVEL_INFO, 'set ' . $value . ' System Setting to ID of ' . $key . ' page');
-            }
-            if ($folderId) {
-                if ($key == 'unsubscribe') {
-                    $resource->set('template', 0);
-
-                } else {
-                    $resource->set('template', $defaultTemplate);
+        /* look for a login page and set the System Setting for it. */
+        /* @var $loginPage modResource */
+        $loginPage = $modx->getObject('modResource',array('alias' => 'login'));
+        if (! $loginPage) {
+            $loginPage = $modx->getObject('modResource',array('alias' => 'Login'));
+        }
+        if (! $loginPage) {
+            $modx->log(xPDO::LOG_LEVEL_ERROR, 'Login page not found - sbs_login_page_id System Settings must be set manually');
+        } else {
+            /* @var $ss modSystemSetting */
+            $ss = $modx->getObject('modSystemSetting', array('key' => 'sbs_login_page_id'));
+            if (!$ss) {
+                $modx->log(xPDO::LOG_LEVEL_ERROR, 'sbs_login_page_id System Setting not found');
+            } else {
+                $ss->set('value', $loginPage->get('id'));
+                if ($ss->save()) {
+                    $modx->log(xPDO::LOG_LEVEL_INFO, 'set sbs_login_page_id System Setting to ID of Login page');
                 }
-                $resource->set('parent', $folderId);
-                $resource->save();
             }
+        }
+       /* set other System Setting values to resource IDs of the subscribe resources
+        * this array is in the form 'alias' =>
+
+       */
+        $settingNames = array(
+             'confirm-register' => 'sbs_confirm_register_page_id',
+             'manage-preferences' => 'sbs_manage_prefs_page_id',
+             'subscribe' => 'sbs_register_page_id',
+             'registration-confirmed' => 'sbs_registration_confirmed_page_id',
+             'thanks-for-registering' => 'sbs_thank_you_page_id',
+             'unsubscribe' => 'sbs_unsubscribe_page_id',
+        );
+
+        foreach($settingNames as $key => $value) {
+            /* @var $systemSetting modSystemSetting */
+            $resource = $modx->getObject('modResource', array ('alias' => $key));
+            $systemSetting = $modx->getObject('modSystemSetting', array ('key' => $value));
+
+            if ($resource && $systemSetting) {
+                $systemSetting->set('value', $resource->get('id'));
+                if ($systemSetting->save()) {
+                    $modx->log(xPDO::LOG_LEVEL_INFO, 'set ' . $value . ' System Setting to ID of ' . $key . ' page');
+                }
+                if ($folderId) {
+                    if ($key == 'unsubscribe') {
+                        $resource->set('template', 0);
+
+                    } else {
+                        $resource->set('template', $defaultTemplate);
+                    }
+                    $resource->set('parent', $folderId);
+                    $resource->save();
+                }
 
 
+            } else {
+                if (! $resource) {
+                    $modx->log(xPDO::LOG_LEVEL_ERROR, 'Unable to get resource with alias: ' . $key);
+                }
+                if (! $systemSetting) {
+                    $modx->log(xPDO::LOG_LEVEL_ERROR, 'Unable to get System Setting with key: ' . $value);
+                }
+            }
+        }
+
+        /* set Secret Key */
+
+        $ss = $modx->getObject('modSystemSetting', array ('key' => 'sbs_secret_key'));
+
+        if ($ss) {
+            $val = $ss->get('value');
+            if (empty($val)) {
+                $k = uniqid('SBS');
+                $ss->set('value', $k);
+                if ($ss->save()) {
+                    $modx->log(xPDO::LOG_LEVEL_INFO, 'set Secret Key System Setting');
+                }
+            }
         } else {
-            if (! $resource) {
-                $modx->log(xPDO::LOG_LEVEL_ERROR, 'Unable to get resource with alias: ' . $key);
-            }
-            if (! $systemSetting) {
-                $modx->log(xPDO::LOG_LEVEL_ERROR, 'Unable to get System Setting with key: ' . $value);
-            }
+            $modx->log(xPDO::LOG_LEVEL_ERROR, 'Unable to get Secret Key System Setting');
         }
-    }
-
-    /* set Secret Key */
-
-    $ss = $modx->getObject('modSystemSetting', array ('key' => 'sbs_secret_key'));
-
-    if ($ss) {
-        $val = $ss->get('value');
-        if (empty($val)) {
-            $k = uniqid('SBS');
-            $ss->set('value', $k);
-            $ss->save();
-        }
-    }
 
 
-    $success = true;
-    break;
+        $success = true;
+        break;
 
     /* This code will execute during an uninstall */
     case xPDOTransport::ACTION_UNINSTALL:
     default:
+        break;
 
 }
 
