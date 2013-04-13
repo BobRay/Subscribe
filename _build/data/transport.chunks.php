ceae6a7a8fcba9b03be009554789b32707c61fc9
bobray <bobray@softville.com>
2013-04-13 16:18:29 -0400

    fix typo in whydialog text tpl chunk name

--- _build/data/transport.chunks.php
+++ _build/data/transport.chunks.php
@@ -5,7 +5,7 @@ $chunks = array();
 $chunks[1] = $modx->newObject('modChunk');
 $chunks[1] ->fromArray(array(
     'id' => 1,
-    'name' => 'sbswhyDialogTextTpl',
+    'name' => 'sbsWhyDialogTextTpl',
     'description' => 'Text for Subscribe Why Dialog',
     'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/sbswhydialogtexttpl.chunk.html'),
     'properties' => '',
